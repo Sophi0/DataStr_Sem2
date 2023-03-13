748edbf4705c18b449d2ae83bfc8b5c99228c559
Sofja Hohlova <s21hohlsofj@vea.lv>
2023-03-13 10:39:19 +0200

    Add: add a position function

--- src/dataStr/MyLinkedList.java
+++ src/dataStr/MyLinkedList.java
@@ -21,7 +21,7 @@ public class MyLinkedList<T> {
 	//TODO: isFull, isEmpty, howManyElements(pirmaja seminara ir)
 	
 	public void add(T newElement) {
-		//TODO: verify  is not full
+		//TODO verify  is not full
 		
 				//list is empty
 				if(elementCounter == 0) {
@@ -30,7 +30,75 @@ public class MyLinkedList<T> {
 					last = newNode;		//norade uz pedejo elementu
 					elementCounter++;	//obligati jaraksta sho, lai palielinas katru reizi
 				}
+				
+				else {
+					MyNode<T> newNode = new MyNode<T>(newElement);	//jauns mezgls
+					last.setNext(newNode); 	//iedodam saiti uz newNode mezgli(67)
+					newNode.setPrevious(last);	//pirms jauna mezgla ir previous mezgls(kur bija 35)
+					last = newNode;
+					elementCounter++;
+					
+					//Получается вначале создаем новый блок-элемент, затем следующий добавляем.
+					
+					/*
+					 * tagad ir jauns mezgls: 35 -> 67
+					 */
+				}
 			}
 	
-	
+	public void add(T newElement, int position) throws Exception {
+		//TODO verify is not full
+		
+		if(position < 0 || position > elementCounter) {
+			throw(new Exception("Wrong position"));
+		}
+		else {
+			//add at the beginning
+			
+			/* katru reizi veidojas jauns mezgls:
+			*				200
+			*			previous -> null
+			*		null <- next
+			*/
+			
+			if(position == 0) {
+				MyNode<T> newNode = new MyNode<T>(newElement);
+				first.setPrevious(newNode);
+				newNode.setNext(first);
+				first = newNode;
+				elementCounter++;
+						
+			}
+			
+			//Здесь же мы в самом начале (перед first) добавляем новый блок к уже существующим блокам(к змейке этой)
+			
+			//add at the end
+			else if(position == elementCounter) {
+				add(newElement);
+			}
+			//Здесь по такому же принципу, как было в начале кода, поэтому просто копируем функцию(делаем это перед last)
+				
+			//add in the middle
+			else {
+				MyNode tempNode = first;	//izveidojam jaunu mainigo, kura ir first
+				for(int i = 0; i < position - 1; i++) {	//position - 1, jo mums piemeram elementi 100 un 67, bet jauno bloku vajag starp shitiem ievietot(pa kreisi), tapes -1
+					tempNode = tempNode.getNext();
+				}
+				
+				MyNode tempNode2 = tempNode.getNext();
+				MyNode<T> newNode = new MyNode<T>(newElement);	//izveidots jauns mezgls, kurs bus strap siem diviem mezgliem
+				
+				tempNode.setNext(newNode);	//norade un newNode
+				newNode.setPrevious(tempNode);	//norade uz tempNode
+				
+				tempNode2.setNext(newNode);
+				newNode.setPrevious(tempNode2);
+				
+				elementCounter++;
+				
+				
+			}
+		}
+	}
 }
+	//ЗАМЕТКА! for each цикл подходит для того, чтоб через ВСЕ элементы пройти
