f1183d73bce01f3b1cc69fe641b3aa62d1822f89
Sofja Hohlova <s21hohlsofj@vea.lv>
2023-03-13 09:42:07 +0200

    Add: new class for Node implementation as MyNode

---
+++ src/dataStr/MyNode.java
@@ -0,0 +1,61 @@
+package dataStr;
+
+public class MyNode<T> {
+	
+	/*
+	 * izveidojam Node bloku(mezglu), kur elementi ir: 35; next; previous:
+	 *    		35
+	 *		previous -> null
+	 * null <- next
+	 */
+	
+
+	private T  element;
+	private MyNode next = null;		//uzreiz pec noklusejuma bus null
+	private MyNode previous = null;	//uzreiz pec noklusejuma bus null
+	
+	
+	//set un get funkcijas
+	public T getElement() {
+		return element;
+	}
+
+
+	public void setElement(T inputElement) {
+		if(inputElement != null) {
+			element = inputElement;
+		}
+		else {
+			element = (T) new Object();	
+		}
+	}
+
+
+	public MyNode getNext() {
+		return next;
+	}
+
+
+	public void setNext(MyNode next) {
+		this.next = next;
+	}
+
+
+	public MyNode getPrevious() {
+		return previous;
+	}
+
+
+	public void setPrevious(MyNode previous) {
+		this.previous = previous;
+	}
+
+
+	public MyNode(T inputElement) {
+		setElement(inputElement);
+	}
+	
+	public String toString() {
+		return "" + element;
+	}
+}
